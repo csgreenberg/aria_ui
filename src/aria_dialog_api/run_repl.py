be173a476be5c49a10e6d95628e9db4dba4f70b0
Greenberg <craig.greenberg@nist.gov>
2024-07-06 22:38:57 -0400

    seperated API import and auth definition

--- src/aria_dialog_api/run_repl.py
+++ src/aria_dialog_api/run_repl.py
@@ -1,38 +1,12 @@
+#
+from repl import repl
+import os
 
+# import API implementation
 from aria_dialog_api_auth_gemini import AuthGemini_ARIADialogAPI as ARDI_API
+# define auth used by API implementation
+AUTH_ENV_VAR = 'API_KEY'
+auth = {'api_key': os.getenv(AUTH_ENV_VAR)}
+#
 
-
-RESTART_CMD_STRING = "!RESTART"
-USER_PROMPT = "You: "
-DS_PROMPT = "Dialog System: "
-
-def request_restart(user_response):
-    return user_response.strip() == RESTART_CMD_STRING
-def repl(auth):
-    welcome_string = \
-        f"Welcome to a simple aria dialog system repl! Engage in a dialog.\nEnter the following "\
-        f"command to restart your dialog: {RESTART_CMD_STRING}. Press Ctrl+D to exit.\n"
-    exit_string = "\nThank you for using a simple aria dialog system repl!"
-    ardi_api = ARDI_API()
-    ardi_api.OpenSession(auth)
-    print(welcome_string)
-    exit = False
-    while exit is False:
-        try:
-            user_response = input(USER_PROMPT)
-        except EOFError:
-            ardi_api.CloseSession(destroy_generator=True)
-            exit = True
-            print(exit_string)
-            continue
-        if request_restart(user_response):
-            ardi_api.CloseSession()
-            ardi_api.OpenSession(auth)
-            print(welcome_string)
-            continue
-        llm_response = ardi_api.GetResponse(user_response)
-        print(f'\n{DS_PROMPT}{llm_response}\n')
-
-if __name__ == '__main__':
-    import os
-    repl({'api_key': os.getenv('API_KEY')})
+repl(ARDI_API, auth={'api_key': os.getenv(AUTH_ENV_VAR)})
