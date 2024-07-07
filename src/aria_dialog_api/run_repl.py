7293e8ff318a69122eb492822514246725c5c53c
Greenberg <craig.greenberg@nist.gov>
2024-07-06 22:42:01 -0400

    bugfix

--- src/aria_dialog_api/run_repl.py
+++ src/aria_dialog_api/run_repl.py
@@ -1,13 +1,11 @@
 #
 from repl import repl
-import os
 
 # import API implementation
 from aria_dialog_api_parrot import Parrot_ARIADialogAPI as ARDI_API
 # define auth used by API implementation
-AUTH_ENV_VAR = 'API_KEY'
-auth = {'api_key': os.getenv(AUTH_ENV_VAR)}
+auth = None
 #
 
-repl(ARDI_API, auth={'api_key': os.getenv(AUTH_ENV_VAR)})
+repl(ARDI_API, auth)
 
