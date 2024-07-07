c40b746d3e501138ef99f06af711789de1e8c470
Greenberg <craig.greenberg@nist.gov>
2024-07-06 22:45:18 -0400

    bug fix

--- src/aria_dialog_api/run_repl.py
+++ src/aria_dialog_api/run_repl.py
@@ -1,12 +1,12 @@
 #
 from repl import repl
-import os
 
 # import API implementation
 from aria_dialog_api_auth_gemini import AuthGemini_ARIADialogAPI as ARDI_API
 # define auth used by API implementation
+import os
 AUTH_ENV_VAR = 'API_KEY'
 auth = {'api_key': os.getenv(AUTH_ENV_VAR)}
 #
 
-repl(ARDI_API, auth={'api_key': os.getenv(AUTH_ENV_VAR)})
+repl(ARDI_API, auth=auth)
