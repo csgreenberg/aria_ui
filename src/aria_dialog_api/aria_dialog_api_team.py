3f15f6ea98dd789521d482c8882b3f7d26b2d8dd
Fiscus <Jonathan Fiscus>
2024-07-10 15:55:45 -0400

    - moved around the dialog api.  The implementation is not called aria_dialog_api_team.py rather than parrot.  The file name does change for others. - Add the base uiux.  This is just a demo but it's tied into the dialog api.  It's not fully implementing the API - Added URL parsing to the app so that a iuser id can get sent byt the experiment controller. - Changed the environment variable to be a dictionary string to make it flexible.

--- src/aria_dialog_api/aria_dialog_api_team.py
+++ src/aria_dialog_api/aria_dialog_api_team.py
@@ -1,7 +1,7 @@
 
 from aria_dialog_api_base import AriaDialogAPI
 
-class Parrot_ARIADialogAPI(AriaDialogAPI):
+class Team_ARIADialogAPI(AriaDialogAPI):
     def OpenSession(self, auth=None):
         return
     def CloseSession(self, destroy_generator=False):
