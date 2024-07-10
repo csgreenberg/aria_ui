780242df8325dddb88983c09729a8eebb1e84936
Fiscus <Jonathan Fiscus>
2024-07-10 16:46:40 -0400

    Fixed the team file name in runrepl

--- src/aria_dialog_api/run_repl.py
+++ src/aria_dialog_api/run_repl.py
@@ -2,7 +2,7 @@
 from repl import repl
 
 # import API implementation
-from aria_dialog_api_parrot import Parrot_ARIADialogAPI as ARDI_API
+from aria_dialog_api_team import Team_ARIADialogAPI as ARDI_API
 # define auth used by API implementation
 auth = None
 #
