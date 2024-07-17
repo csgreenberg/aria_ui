d4ff46b4c149ff7ef288a0c4ba29840b3bcc0f50
Greenberg <craig.greenberg@nist.gov>
2024-07-16 23:44:08 -0400

    restored parrot API implementation

--- src/aria_ui/aria_dialog_api_team.py
+++ src/aria_ui/aria_dialog_api_team.py
@@ -1,29 +1,15 @@
 
-from aria_dialog_api_base import AriaDialogAPI
-import google.generativeai as genai
 from utils import convert_text_to_html
+from aria_dialog_api_base import AriaDialogAPI
 
 class Team_ARIADialogAPI(AriaDialogAPI):
-    def __init__(self):
-        self.generator = None
-        self.chat = None
     def OpenConnection(self, auth=None):
-        if not self.generator:
-            genai.configure(api_key=auth['api_key'])
-            self.generator = genai.GenerativeModel('gemini-1.5-flash')
         return True
-    def CloseConnection(self):
-        self.generator = None
-        self.chat = None
+    def CloseConnection(self, destroy_generator=False):
         return True
-    def StartSession(self):
-        self.chat = self.generator.start_chat(history=[])
+    def StartSession():
         return True
     def GetResponse(self, text):
-        response = self.chat.send_message(text, stream=True)
-        text = ''
-        for chunk in response:
-            text += chunk.text
         htmltext = convert_text_to_html(text)
         return {'success': True,
                 'response': htmltext}
