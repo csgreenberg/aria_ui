d3eb4f82e0841e9857db440916f192074cf8163e
Greenberg <craig.greenberg@nist.gov>
2024-08-12 12:06:15 -0400

    added docstrings to aria_dialog_api_team.py

--- src/aria_ui/aria_dialog_api_team.py
+++ src/aria_ui/aria_dialog_api_team.py
@@ -4,23 +4,107 @@ import google.generativeai as genai
 from utils import convert_text_to_html
 
 class Team_ARIADialogAPI(AriaDialogAPI):
+    """This is a simple example of a class that inherits AriaDialogAPI and implements the ARIA
+       dialog API for a toy model that echos back whatever prompt is given.
+
+    Attributes
+    ----------
+    generator : GenerativeModel
+        a class that provides to access the gemini API
+    chat : ChatSession:
+        a class in the google.generativeai module that contains an ongoing conversation with the
+        model
+    Methods
+    -------
+    OpenConnection(auth=None)
+        Opens a connection to the application.
+    CloseConnection()
+        Closes an open connection to the application.
+    GetVersion():
+        Returns the version of the API implementation.
+    StartSession():
+        Starts a new dialog session.
+    GetResponse(text):
+        Returns a response to text prompt.
+    """
+    model_name = 'Gemini'
     def __init__(self):
         self.generator = None
         self.chat = None
     def OpenConnection(self, auth=None):
+        """Opens a connection to the application.
+
+       The parameter `auth` should contain a key 'api_key' with value set to a Gemini API_KEY
+
+       Parameters
+       ----------
+       auth : dict, optional
+           the authorization dictionary that must contain a Gemini API key as a value
+       Returns
+       -------
+       bool
+           a boolean value indicating whether the connection was successfully opened
+       """
         genai.configure(api_key=auth['api_key'])
         self.generator = genai.GenerativeModel('gemini-1.5-flash')
         return True
     def CloseConnection(self):
+        """Closes an open connection to the application.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        bool
+            a boolean value indicating whether the connection was successfully opened. Since there
+            is no connection made for the Echo mode, simply returns True.
+        """
         self.generator = None
         self.chat = None
         return True
     def GetVersion(self):
+        """Returns the version of the API implementation.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        string
+            a string indicating the version of the API implementation.
+        """
         return '0.1'
     def StartSession(self):
+        """Starts a new dialog session.
+
+       Parameters
+       ----------
+       None
+
+       Returns
+       -------
+       bool
+           a boolean value indicating whether the session was successfully started.
+       """
         self.chat = self.generator.start_chat(history=[])
         return True
     def GetResponse(self, text):
+        """Returns a response to text prompt.
+
+         Parameters
+         ----------
+         text : str
+             The prompt from the user to be provided to the model
+
+         Returns
+         -------
+         dictionary
+             a dictionary with keys "success" and "response" with values indicating whether the app
+             successfully returned a response and the response itself in html format, respectively.
+         """
         response = self.chat.send_message(text, stream=True)
         text = ''
         for chunk in response:
